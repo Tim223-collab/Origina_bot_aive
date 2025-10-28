d0c6072abdd4a9a544c58a27208401fa470a72d8
Tim223-collab <miwzua98@gmail.com>
2025-10-28 23:28:24 +0200

    2328

--- services/__init__.py
+++ services/__init__.py
@@ -4,6 +4,10 @@
 from .ai_service import AIService
 from .memory_service import MemoryService
 from .extras_service import ExtrasService
+from .work_parser_service import WorkParserService
 
-__all__ = ["AIService", "MemoryService", "ExtrasService"]
+# Алиас для обратной совместимости
+WorkSiteParser = WorkParserService
+
+__all__ = ["AIService", "MemoryService", "ExtrasService", "WorkParserService", "WorkSiteParser"]
 
